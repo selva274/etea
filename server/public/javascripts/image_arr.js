1472821158c3a3d8c18267415540db9e902838b6
Selvakumar <selvakumard274@gmail.com>
2024-04-01 20:17:35 +0530

    server add

---
+++ server/public/javascripts/image_arr.js
@@ -0,0 +1,56 @@
+const tea = [
+    {
+        name: "Black",
+        image: "images/black.jpeg"
+    },
+    {
+        name: "Mate",
+        image: "images/mate.jpeg"
+    },
+    {
+        name: "Darjeeling",
+        image: "images/darjeeling tea.jpeg"
+    },
+    {
+        name: "Green",
+        image: "images/green-tea.jpg"
+    },
+    {
+        name: "Herbal",
+        image: "images/herbal.jpeg"
+    },
+    {
+        name: "Jasmine",
+        image: "images/jasmine.jpeg"
+    },
+    {
+        name: "Matcha",
+        image: "images/matcha.jpeg"
+    },
+    {
+        name: "Oolong",
+        image: "images/oolong.jpg"
+    },
+    {
+        name: "Rooibos",
+        image: "images/rooibos.webp"
+    },
+    {
+        name: "Sencha",
+        image: "images/sencha.jpg"
+    },
+    {
+        name: "White",
+        image: "images/white.webp"
+    },
+    {
+        name: "Yellow",
+        image: "images/yellow.jpeg"
+    },
+
+]
+
+exports.module = tea;
+
+
+
